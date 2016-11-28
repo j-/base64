5813742da83474a2de57686fefee0b03a4b9d57c
j- <j@skeoh.com>
2016-11-28 11:15:21 +1100

    Add App component

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,11 @@
 import '@blueprintjs/core/dist/blueprint';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+);
