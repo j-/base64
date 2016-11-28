6bb4a070dc3b7f20b45d1231852c850ab1d1e68d
j- <j@skeoh.com>
2016-11-28 14:06:07 +1100

    Add header and resize app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import Convert from './Convert';
 
 const App = () => (
-	<div>
+	<div className="app">
+		<h1>Base 64</h1>
 		<Convert />
 	</div>
 );
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 import '@blueprintjs/core/dist/blueprint';
+import './styles';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
