24111b15cd76a4c339a0e6a3da0678b70b7ff249
j- <j@skeoh.com>
2016-11-28 13:07:44 +1100

    Perform conversion when changing values

--- src/components/Convert.jsx
+++ src/components/Convert.jsx
@@ -5,17 +5,54 @@ export default class Convert extends Component {
 	constructor (props) {
 		super(props);
 		this.state = {
-			fromValue: 'aGVsbG8gd29ybGQ=',
+			valueLeft: '',
+			valueRight: '',
+			calculateLeft: false,
+			calculateRight: false,
 		};
+		this.handleChangeLeft = this.handleChangeLeft.bind(this);
+		this.handleChangeRight = this.handleChangeRight.bind(this);
+	}
+
+	handleChangeLeft (valueLeft) {
+		this.setState({
+			valueLeft,
+			calculateLeft: false,
+			calculateRight: true,
+		});
+	}
+
+	handleChangeRight (valueRight) {
+		this.setState({
+			valueRight,
+			calculateLeft: true,
+			calculateRight: false,
+		});
+	}
+
+	getValueLeft () {
+		if (this.state.calculateLeft) {
+			return btoa(this.state.valueRight);
+		}
+		return this.state.valueLeft;
+	}
+
+	getValueRight () {
+		if (this.state.calculateRight) {
+			return atob(this.state.valueLeft);
+		}
+		return this.state.valueRight;
 	}
 
 	render () {
-		const { fromValue } = this.state;
-		const toValue = atob(fromValue);
+		const valueLeft = this.getValueLeft();
+		const valueRight = this.getValueRight();
 		return (
 			<DualText
-				valueLeft={ fromValue }
-				valueRight={ toValue }
+				valueLeft={ valueLeft }
+				onChangeLeft={ this.handleChangeLeft }
+				valueRight={ valueRight }
+				onChangeRight={ this.handleChangeRight }
 				primaryLeft
 			/>
 		);
