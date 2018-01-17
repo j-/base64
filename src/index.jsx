782b207a588c811a6ec9fef263528ebfa981739a
j- <j@skeoh.com>
2018-01-18 09:51:24 +1100

    Add Redux devtools extension support

--- src/index.jsx
+++ src/index.jsx
@@ -4,12 +4,13 @@ import './styles';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 
 import App from './containers/App';
 import reducer from './store';
 
-const store = createStore(reducer);
+const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
 	<Provider store={ store }>
