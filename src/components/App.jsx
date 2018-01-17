328d11839232d4ab59e350acdf1dd9ffc2c18b92
j- <j@skeoh.com>
2018-01-18 09:45:48 +1100

    Install prop-types and clear deprecation warning

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Ribbon from './Ribbon';
 import ConversionList from './ConversionListContainer';
 
--- src/components/DualText.jsx
+++ src/components/DualText.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import FlexContainer from './FlexContainer';
 import FlexItem from './FlexItem';
 import SingleText from './SingleText';
